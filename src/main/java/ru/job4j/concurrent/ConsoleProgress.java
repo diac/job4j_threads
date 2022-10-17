d98f54b2b12f82d68fe422f17ad1cbbdda43c74d
diac <nikolai.gladkikh@gmail.com>
2022-10-17 12:38:10 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,35 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    private static final char[] SPINNER = new char[] {'-', '\\', '|', '/'};
+
+    @Override
+    public void run() {
+        byte i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            if (i == SPINNER.length) {
+                i = 0;
+            }
+            System.out.print("\r load: " + SPINNER[i++]);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("The process has been interrupted");
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt();
+    }
+}
