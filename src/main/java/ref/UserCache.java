5bdef8e4ca9a0a391995530451d2a1ce2e32528e
diac <nikolai.gladkikh@gmail.com>
2022-10-18 12:18:22 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ref/UserCache.java
@@ -0,0 +1,30 @@
+package ref;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import net.jcip.annotations.NotThreadSafe;
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        return new ArrayList<>(
+                users.values().stream()
+                        .map(user -> User.of(user.getName()))
+                        .toList()
+        );
+    }
+}
