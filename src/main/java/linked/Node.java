cb7b2bd0821507d0a67252d80f538fce3f0dd080
diac <nikolai.gladkikh@gmail.com>
2022-10-18 11:50:39 +0300

    3. Immutable объекты [#267918]

---
+++ src/main/java/linked/Node.java
@@ -0,0 +1,20 @@
+package linked;
+
+public class Node<T> {
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    private final Node<T> next;
+    private final T value;
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
