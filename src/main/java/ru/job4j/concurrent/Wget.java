1588732bb703bfcb697db9b247a979f0a9f2d4f7
diac <nikolai.gladkikh@gmail.com>
2022-10-18 11:42:06 +0300

    4. Скачивание файла с ограничением. [#144271 #352431]: Добавить расчет времени в паузах

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -5,9 +5,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
-import static ru.job4j.concurrent.WgetArgsValidator.*;
+import static ru.job4j.concurrent.WgetArgsValidator.validateArgs;
 
 public class Wget implements Runnable {
+
+    private static final int SECOND = 1000;
+
     private final String url;
     private final int speed;
 
@@ -18,16 +21,31 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(getOutputFileName(url))) {
+        try (
+                BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
+                FileOutputStream fileOutputStream = new FileOutputStream(getOutputFileName(url))
+        ) {
+            long startTime = System.currentTimeMillis();
+            long timePerStep;
+            long pauseMillis;
             byte[] dataBuffer = new byte[speed];
             int bytesRead;
+            int downloadedBytes = 0;
             while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                downloadedBytes += bytesRead;
+                if (downloadedBytes >= speed) {
+                    timePerStep = System.currentTimeMillis() - startTime;
+                    if (timePerStep < SECOND) {
+                        pauseMillis = SECOND - timePerStep;
+                        try {
+                            Thread.sleep(pauseMillis);
+                            startTime = System.currentTimeMillis();
+                            downloadedBytes = 0;
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             }
         } catch (IOException e) {
