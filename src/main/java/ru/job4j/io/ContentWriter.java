d41d874450025fcda174f553d4fab415312f21a8
diac <nikolai.gladkikh@gmail.com>
2022-10-19 10:13:43 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102 #352844]: Исправить замечания по классу ContentWriter

--- src/main/java/ru/job4j/io/ContentWriter.java
+++ src/main/java/ru/job4j/io/ContentWriter.java
@@ -1,14 +1,17 @@
 package ru.job4j.io;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
-public class ContentWriter {
+public final class ContentWriter {
 
-    public void saveContent(String content, File file) throws IOException {
-        try (OutputStream o = new FileOutputStream(file)) {
+    private final File file;
+
+    public ContentWriter(File file) {
+        this.file = file;
+    }
+
+    public synchronized void saveContent(String content) throws IOException {
+        try (OutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
             for (int i = 0; i < content.length(); i += 1) {
                 o.write(content.charAt(i));
             }
