f15125ab0d5cfc8acf7ade1789c846f6d05b4f88
diac <nikolai.gladkikh@gmail.com>
2022-10-19 12:25:04 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
@@ -3,10 +3,47 @@ package ru.job4j.concurrent;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
 
 class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> IntStream.range(0, 5).forEach(
+                        i -> {
+                            try {
+                                queue.offer(i);
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                            }
+                        }
+                )
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).containsExactly(0, 1, 2, 3, 4);
+    }
 
     @Test
     public void whenPollFromOverflow() throws InterruptedException {
@@ -32,8 +69,10 @@ class SimpleBlockingQueueTest {
         });
         consumer.start();
         producer.start();
-        consumer.join();
         producer.join();
+        consumer.interrupt();
+        consumer.join();
+
         assertThat(List.of(queue.poll(), queue.poll(), queue.poll())).containsExactly(3, 4, 5);
     }
 }
